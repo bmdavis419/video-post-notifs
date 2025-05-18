62773d3eff2de814a51daab64805c1426bed6df0
Benjamin Davis <davis.benjamin41902@gmail.com>
2025-05-18 01:39:53 -0400

    core logic done

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,7 @@ function parseLatest(xml: string): VideoInfo | null {
 	return { id, title, description };
 }
 
-const getRssFeed = async () => {
+const getRssForVideo = async () => {
 	const CHANNEL_ID = 'UCbRP3c757lWg9M-U7TyEkXA';
 
 	const rssUrl = `https://www.youtube.com/feeds/videos.xml?channel_id=${CHANNEL_ID}`;
@@ -62,6 +62,45 @@ const getRssFeed = async () => {
 	const video = parseLatest(xml);
 
 	return video;
+};
+
+const prepareMessage = (video: VideoInfo) => {
+	return `New video uploaded: **${video.title}**\n\n${video.description}`;
+};
+
+async function sendDiscordMessage(props: { message: string; env: Env }): Promise<void> {
+	const { DISCORD_TOKEN, DISCORD_CHANNEL_ID } = props.env;
+	const url = `https://discord.com/api/v10/channels/${DISCORD_CHANNEL_ID}/messages`;
+
+	const clean = props.message.replace(/(https?:\/\/[^\s]+)/g, (url) => `\`${url}\``);
+
+	const res = await fetch(url, {
+		method: 'POST',
+		headers: {
+			Authorization: `Bot ${DISCORD_TOKEN}`,
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ content: clean }),
+	});
+
+	if (!res.ok) {
+		const body = await res.text();
+		throw new Error(`Discord API error [${res.status}]: ${body || res.statusText}`);
+	}
+}
+
+const checkCacheForVideo = async (props: { videoId: string; env: Env }) => {
+	const { videoId, env } = props;
+
+	const video = await env.KV_CHANNEL_UPLOAD_BOT.get(videoId);
+
+	return video;
+};
+
+const saveVideoToCache = async (props: { video: VideoInfo; env: Env }) => {
+	const { video, env } = props;
+
+	await env.KV_CHANNEL_UPLOAD_BOT.put(video.id, JSON.stringify(video));
 };
 
 export default {
@@ -75,14 +114,26 @@ export default {
 	// The scheduled handler is invoked at the interval set in our wrangler.jsonc's
 	// [[triggers]] configuration.
 	async scheduled(event, env, ctx): Promise<void> {
-		const rss = await getRssFeed();
+		const videoData = await getRssForVideo();
 
-		console.log(rss);
+		if (!videoData) {
+			console.error('No video data found, error fetching from rss');
+			return;
+		}
 
-		let wasSuccessful = rss ? 'success' : 'fail';
+		const hasSent = await checkCacheForVideo({ videoId: videoData.id, env });
 
-		// You could store this result in KV, write to a D1 Database, or publish to a Queue.
-		// In this template, we'll just log the result:
-		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+		if (!!hasSent) {
+			console.log('Video already sent, skipping');
+			return;
+		}
+
+		const message = prepareMessage(videoData);
+
+		await sendDiscordMessage({ message, env });
+
+		await saveVideoToCache({ video: videoData, env });
+
+		console.log('Video sent and cached');
 	},
 } satisfies ExportedHandler<Env>;
